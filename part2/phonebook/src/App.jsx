3c9fbe675daf90da86774d3e5c65985693948bb6
Jan <jan.zwank@web.de>
2026-01-25 14:29:05 +0100

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -11,12 +11,19 @@ const Person = (props) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123-456-7890" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+
+  let currentIndex = 4;
 
   const [newName, setNewName] = useState("");
 
   const [newNumber, setNewNumber] = useState("");
+
+  const [filterValue, setFiltervalue] = useState("");
 
   const checkForDuplicate = () => {
     const isIncluded = persons.reduce(
@@ -25,6 +32,10 @@ const App = () => {
       false,
     );
     return isIncluded;
+  };
+
+  const handleFilterChange = (event) => {
+    setFiltervalue(event.target.value);
   };
 
   const handleNameInputChange = (event) => {
@@ -41,14 +52,22 @@ const App = () => {
       alert(`${newName} is already in Phonebook!`);
       return;
     }
-    setPersons(persons.concat({ name: newName, number: newNumber }));
+    currentIndex++;
+    setPersons(
+      persons.concat({ id: currentIndex, name: newName, number: newNumber }),
+    );
     setNewName("");
     setNewNumber("");
   };
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with{" "}
+        <input value={filterValue} onChange={handleFilterChange} />
+      </div>
+      <h2>New Entry</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameInputChange} />
@@ -63,9 +82,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <Person key={person.name} name={person.name} number={person.number} />
-      ))}
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().startsWith(filterValue.toLowerCase()),
+        )
+        .map((person) => (
+          <Person key={person.id} name={person.name} number={person.number} />
+        ))}
     </div>
   );
 };
