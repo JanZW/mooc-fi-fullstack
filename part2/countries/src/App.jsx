665382942ee627e47c6f1d040bf3e47f5892871b
Jan <jan.zwank@web.de>
2026-01-25 20:23:33 +0100

    Exercise 2.19*

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,23 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const CountryDetails = ({country}) => {
+const CountryDetails = ({ country }) => {
+  if (country == null) {
+    return null;
+  }
   return (
     <div>
-    <h2>{country.name.common}</h2>
-    <p>Capital: {country.capital}<br/>
-    Area: {country.area}</p>
-    <h3>Languages</h3>
-    <ul>
-    {Object.keys(country.languages).map(lang => <li key={lang}>{country.languages[lang]}</li>)}
-    </ul>
-    <img src={country.flags.png} alt={country.flags.alt} />
+      <h2>{country.name.common}</h2>
+      <p>
+        Capital: {country.capital}
+        <br />
+        Area: {country.area}
+      </p>
+      <h3>Languages</h3>
+      <ul>
+        {Object.keys(country.languages).map((lang) => (
+          <li key={lang}>{country.languages[lang]}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt} />
     </div>
   );
-}
+};
 
-const Countries = ({ countries, filterString }) => {
-  console.log('rendering Countries')
+const Countries = ({ countries, filterString, setSelectedCountry }) => {
   if (countries == null) {
     return null;
   }
@@ -25,34 +32,58 @@ const Countries = ({ countries, filterString }) => {
     country.name.common.toLowerCase().includes(filterString.toLowerCase()),
   );
 
+  console.log(filteredCountries);
+
   if (filteredCountries.length > 10) {
+    setSelectedCountry(null);
     return (
-      <p>Too many matches ({filteredCountries.length}), specify another filter</p>
-    )
+      <p>
+        Too many matches ({filteredCountries.length}), specify another filter
+      </p>
+    );
   }
 
   if (filteredCountries.length === 0) {
-    return 'No Matches'
+    return "No Matches";
   }
 
   if (filteredCountries.length === 1) {
-    return <CountryDetails country={filteredCountries[0]}/>
+    setSelectedCountry(filteredCountries[0]);
+    return null;
   }
 
-  return filteredCountries.map((country) => (
-    <li key={country.cca2}>{country.name.common}</li>
-  ));
+  return (
+    <>
+      {filteredCountries.map((country) => (
+        <div key={country.cca2}>
+          <li>
+            {country.name.common}{" "}
+            <button
+              onClick={(event) => {
+                console.log(`show country ${country}`);
+                setSelectedCountry(country);
+              }}
+            >
+              Show
+            </button>
+          </li>
+        </div>
+      ))}
+    </>
+  );
 };
 
 const App = () => {
   const [countries, setCountries] = useState(null);
   const [searchString, setSearchString] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   const handleSearchString = (event) => {
     setSearchString(event.target.value);
+    setSelectedCountry(null);
   };
   useEffect(() => {
-    console.log('effect')
+    console.log("effect");
     axios
       .get("https://studies.cs.helsinki.fi/restcountries/api/all")
       .then((response) => {
@@ -61,13 +92,20 @@ const App = () => {
       .catch((error) => console.log(error));
   }, []);
 
+  console.log(selectedCountry);
+
   return (
     <>
       <h1>Countries</h1>
       find countries
       <input value={searchString} onChange={handleSearchString} />
       <br />
-      <Countries countries={countries} filterString={searchString}/>
+      <Countries
+        countries={countries}
+        filterString={searchString}
+        setSelectedCountry={setSelectedCountry}
+      />
+      <CountryDetails country={selectedCountry} />
     </>
   );
 };
