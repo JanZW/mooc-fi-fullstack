6cebe382d00af9573d2dd19674b48e83100d2548
Jan <jan.zwank@web.de>
2026-01-25 14:18:05 +0100

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,58 +1,73 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Person = (props) => {
   return (
     <>
-      { props.name }
-      <br/>
+      {props.name}: {props.number}
+      <br />
     </>
-  )
-}
+  );
+};
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: "Arto Hellas", number: "123-456-7890" },
+  ]);
 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState("");
+
+  const [newNumber, setNewNumber] = useState("");
 
   const checkForDuplicate = () => {
     const isIncluded = persons.reduce(
-      (isIncludedAcc, currentPerson) => isIncludedAcc || currentPerson.name === newName,
-      false
-    )
-    return isIncluded
-  }
+      (isIncludedAcc, currentPerson) =>
+        isIncludedAcc || currentPerson.name === newName,
+      false,
+    );
+    return isIncluded;
+  };
 
-  const handleInputChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameInputChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberInputChange = (event) => {
+    setNewNumber(event.target.value);
+  };
 
   const handleClickSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (checkForDuplicate()) {
-      alert(`${newName} is already in Phonebook!`)
-      return
+      alert(`${newName} is already in Phonebook!`);
+      return;
     }
-    setPersons(persons.concat({name:newName}))
-    setNewName('')
-  }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName("");
+    setNewNumber("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleInputChange}/>
+          name: <input value={newName} onChange={handleNameInputChange} />
         </div>
         <div>
-          <button type="submit" onClick={handleClickSubmit}>add</button>
+          number: <input value={newNumber} onChange={handleNumberInputChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleClickSubmit}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      { persons.map(person => <Person key={person.name} name={person.name} />) }
+      {persons.map((person) => (
+        <Person key={person.name} name={person.name} number={person.number} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
