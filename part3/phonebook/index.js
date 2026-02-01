65e070d753619ee9fe000e47748684d8cb5da68f
Jan <jan.zwank@web.de>
2026-02-01 14:33:53 +0100

    Exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,33 +1,44 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons);
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-const PORT = 3001
-app.listen(3001, () => {console.log(`Running on port ${PORT}`)})
+app.get("/info", (request, response) => {
+  const date = new Date();
+  console.log(date.toString());
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>` +
+      `<p>${date.toString()}</p>`,
+  );
+});
+
+const PORT = 3001;
+app.listen(3001, () => {
+  console.log(`Running on port ${PORT}`);
+});
