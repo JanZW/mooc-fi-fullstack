6127024b27684259ca5a885f88693ed4e499079f
Jan <jan.zwank@web.de>
2026-02-08 17:08:56 +0100

    Exercise 3.16

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -39,35 +39,31 @@ let persons = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((people) => response.json(people));
+app.get("/api/persons", (request, response, next) => {
+  Person.find({}).then((people) => response.json(people)).catch(error => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id).then((person) => {
     if (!person) {
       response.status(404);
     } else {
       response.json(person);
     }
-  });
+  }).catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const personId = request.params.id;
 
   Person.findByIdAndDelete(request.params.id)
       .then(result => {
         response.status(204).end()
       })
-      .catch(error => {
-        console.log(error),
-        response.status(500).end()
-        }
-      )
+      .catch(error => next(error))
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const newPerson = request.body;
 
   if (!newPerson) {
@@ -82,7 +78,7 @@ app.post("/api/persons", (request, response) => {
     name: newPerson.name, number: newPerson.number
   })
 
-  person.save().then(savedPerson => response.json(savedPerson))
+  person.save().then(savedPerson => response.json(savedPerson)).catch(error => next(error))
 });
 
 app.get("/info", (request, response) => {
@@ -92,6 +88,19 @@ app.get("/info", (request, response) => {
       `<p>${date.toString()}</p>`,
   );
 });
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3001;
 app.listen(3001, () => {
